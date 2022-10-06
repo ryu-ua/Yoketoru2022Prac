1f5e8e8cde042c12b347aef2e882039bb237b77c
A601-01 <tnk.dat.class@gmail.com>
2022-10-06 15:09:43 +0900

    ゲーム開始

---
+++ Assets/Yoketoru/Scripts/ToNextScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//継承= ほかのクラスの機能を受け継いで拡張
+public class ToNextScene : MonoBehaviour
+{
+    [Tooltip("切り替えたいシーン名"), SerializeField]
+    string nextScene = default;
+
+    bool sceneChanged;
+
+    public void ChangeScene()
+    {
+        if (sceneChanged) return;
+
+        sceneChanged = true;
+        TinyAudio.PlaySE(TinyAudio.SE.Start);
+        SceneManager.LoadScene(nextScene);
+    }
+
+}
