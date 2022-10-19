238000621b0f99001fe972ba55545baa3877e7e3
A601-01 <tnk.dat.class@gmail.com>
2022-10-19 10:18:21 +0900

    マウス移動

--- Assets/Yoketoru/Scripts/Player.cs
+++ Assets/Yoketoru/Scripts/Player.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float cameraDistance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
         var mpos = Input.mousePosition;
-        Debug.Log(mpos);
+        //Debug.Log(mpos);
+        mpos.z = cameraDistance;
+        var wp = Camera.main.ScreenToWorldPoint(mpos);
+        transform.position = wp;
     }
 }
